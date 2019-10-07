2218c54cc0354edde3e0d786692d3f26bdc8920e
Chase Kimball <charleskimball2022@u.northwestern.edu>
2019-10-07 17:35:55 -0500

    typo

--- dynamical_population_models/models.py
+++ dynamical_population_models/models.py
@@ -315,7 +315,7 @@ class EmpiricalBranchingFractionNoSpin(EmpiricalBranchingFraction):
 
 
 def low_spin_component(spin):
-    return (spin >= 0.05).astype(float) / 0.05
+    return (spin <= 0.05).astype(float) / 0.05
 
 
 def first_generation_spin_magnitude(spin, alpha, beta, delta, a_max):
